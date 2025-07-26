f4cad61efbeaaec4caf70ba356accf29afd4f677
MKNitro88 <mati88kaplan@outlook.com>
2025-07-26 20:25:11 +0300

    Initial commit

---
+++ src/main/java/dto/ErrorMessageDto.java
@@ -0,0 +1,17 @@
+package dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessageDto {
+    private String timestamp;
+    private int status;
+    private String error;
+    private Object message;
+    private String path;
+}
